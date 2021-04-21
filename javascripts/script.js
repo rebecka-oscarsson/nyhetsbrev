9db5ef47c173bfd6ef5c07ffbcff15621f84b750
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2021-04-21 10:44:40 +0200

    login logout and newsletter + database

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,9 +1,20 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-let activeUser = urlParams.get("id");
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+// let activeUser = urlParams.get("id");
 //är det ok att skicka använar id:t här? tror det
 
-localStorage.setItem("savedActiveUser", activeUser);
+//hämtar eventuell inloggad användare från localStorage
+let activeUser;
+let savedActiveUser = localStorage.getItem("savedActiveUser");
+//problem: denna hämtas och skriver över
+
+
+//kollar om det finns en inloggad användare lagrad, i så fall använd den
+if (savedActiveUser) {
+    activeUser = savedActiveUser
+};
+
+// localStorage.setItem("savedActiveUser", activeUser);
 //skicka ett id på inloggad anv och spara i localstorage. sedan använd för att hämta användarnamn och nyhetsbrev
 
 function getData(url, callbackFunction) {
@@ -16,11 +27,28 @@ function getData(url, callbackFunction) {
         })
 }
 
+function postData(url, dataToSend, callbackFunction) {
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataToSend)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (callbackFunction) {
+                callbackFunction(data)
+            }
+        })
+}
+
+
 // let users = getData("./savedUsers", logData);
 // let activeUser = getData("./activeUser", logData);
 
 function logData(data) {
-    console.log(data)
+    console.log("svar från login", JSON.parse(data))
 }
 
 //hämtar eventuella sparade användare samt inloggad användare från localStorage
@@ -44,10 +72,22 @@ const menu = document.getElementsByTagName("section")[0];
 const contents = document.getElementsByTagName("section")[1];
 const contentsHeadline = document.getElementsByTagName("h2")[1];
 
+
+function login(event){
+    event.preventDefault();//onödigt?
+    let formData = new FormData(document.getElementById("loginForm"));
+    const formObject = Object.fromEntries(formData.entries());
+    postData("http://localhost:3000/users/login", formObject, saveUser)
+}
+
+function saveUser(user) {user = JSON.parse(user); alert(user); activeUser = user; if(user && user!="incorrect"){localStorage.setItem("savedActiveUser", user)};
+ printPage(user)};
+
+
 //tre olika innehåll som kan visas i menydelen av sidan
-const menuLogin =
+const menuLogin =//ändra så att det ligger en eventlistener på loginBtn.action="http://localhost:3000/users/login" method="post"
     `<h3>Logga in</h3><label for="username">Användare:</label>
-<form action="http://localhost:3000/users/login" method="post">
+<form id="loginForm" method="post">
 <input type="text" id="username" name="name" required>
 <label for="password">Lösenord:</label>
 <input type="password" id="password" name="pwd" required>
@@ -102,7 +142,7 @@ printPage(activeUser)
  lägger till eventlisteners som kör funktionerna login, logout och printRegisterForm*/
 function printPage(activeUser) {
     console.log("activeUser: ", activeUser)
-    if (!activeUser) {
+    if (activeUser=="undefined"||activeUser==undefined||activeUser==null) {
         contents.textContent = "Logga in eller registrera dig till vänster";
         menu.innerHTML = "";
         menu.insertAdjacentHTML("afterbegin", menuLogin);
@@ -111,14 +151,20 @@ function printPage(activeUser) {
             menu.innerHTML = "";
             menu.insertAdjacentHTML("afterbegin", menuRegister)
         });
+        const loginForm = document.getElementById("loginForm")
+        loginForm.addEventListener("submit", login);
     } else if (activeUser === "incorrect") {
         contents.innerHTML = "";
         contents.insertAdjacentHTML("afterbegin", contentsError);
         menu.innerHTML = "";
         menu.insertAdjacentHTML("afterbegin", menuLogin);
         const registerLink = document.querySelector("a");
-        registerLink.addEventListener("click", printRegisterForm);
+        registerLink.addEventListener("click", () => {
+            menu.innerHTML = "";
+            menu.insertAdjacentHTML("afterbegin", menuRegister)
+        });
     } else {
+        activeUser = activeUser.replace(/^"(.*)"$/, '$1');//fullösning för att få bort citat
         getData("http://localhost:3000/users/userData/" + activeUser, printLoggedInPage);
     }
 }
@@ -150,6 +196,7 @@ Nuvarande inställning: <span id="setting">${currentSetting}</span>
     checkbox.addEventListener("change", () => {
         changeNewsletter(checkbox.checked)
     })
+    document.getElementById("logoutBtn").addEventListener("click", ()=>{activeUser=null; localStorage.removeItem("savedActiveUser")})
 }
 
 function changeNewsletter(newSetting) {
@@ -158,24 +205,40 @@ function changeNewsletter(newSetting) {
         "id": savedActiveUser,
         "newsletter": newSetting
     }
-    fetch("http://localhost:3000/users/changeNewsLetter", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(prenumeration)
-        })
-        .then(res => res.json())
-        .then(data => {
-            const setting = document.getElementById("setting");
-            if (data==true) {
-                setting.textContent = "Prenumererar!";
-            }
-            else if (data =="error") {
-                setting.textContent = "Användaren hittades inte";
-            }
-            else {
-                setting.textContent = "Prenumererar inte";
-            }
-        });
+
+    postData("http://localhost:3000/users/changeNewsLetter", prenumeration, printNewsletterSetting)
+
+    // fetch("http://localhost:3000/users/changeNewsLetter", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(prenumeration)
+    //     })
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         const setting = document.getElementById("setting");
+    //         if (data==true) {
+    //             setting.textContent = "Prenumererar!";
+    //         }
+    //         else if (data =="error") {
+    //             setting.textContent = "Användaren hittades inte";
+    //         }
+    //         else {
+    //             setting.textContent = "Prenumererar inte";
+    //         }
+    //     });
+}
+
+function printNewsletterSetting(data){//den här funktionen måste gå att ta bort
+    const setting = document.getElementById("setting");
+    if (data==true) {
+        setting.textContent = "Prenumererar!";
+    }
+    else if (data =="error") {
+        setting.textContent = "Användaren hittades inte";
+    }
+    else {
+        setting.textContent = "Prenumererar inte";
+    }
 }
